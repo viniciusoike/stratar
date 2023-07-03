bbb9a9c0e2877aa535b2a9ed4e3753d0f0bef051
viniciusoike <viniciusoike@gmail.com>
2023-07-03 16:48:52 -0300

    Create shapes.R

---
+++ data-raw/shapes.R
@@ -0,0 +1,5 @@
+## code to prepare `shapes` dataset goes here
+
+# wgt10 <- geobr::read_weighting_area(year = 2010, simplified = FALSE)
+# wgt10 <- dplyr::select(wgt10, code_weighting)
+# usethis::use_data(wgt10, overwrite = TRUE)
